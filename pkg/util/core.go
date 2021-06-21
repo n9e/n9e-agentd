efdda06a505db5b4bd57dec675f929be96cd2e5f
yubo <yubo@yubo.org>
2021-06-21 10:57:11 +0800

    add core

---
+++ pkg/util/core.go
@@ -0,0 +1,26 @@
+// +build !windows
+
+package util
+
+import (
+	"fmt"
+	"runtime/debug"
+
+	"golang.org/x/sys/unix"
+)
+
+// SetupCoreDump enables core dumps and sets the core dump size limit based on configuration
+func SetupCoreDump() error {
+	debug.SetTraceback("crash")
+
+	err := unix.Setrlimit(unix.RLIMIT_CORE, &unix.Rlimit{
+		Cur: unix.RLIM_INFINITY,
+		Max: unix.RLIM_INFINITY,
+	})
+
+	if err != nil {
+		return fmt.Errorf("Failed to set ulimit for core dumps: %s", err)
+	}
+
+	return nil
+}
